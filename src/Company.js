5f8cd59ce2b42b8d38b06ebff1627fe9d9ed8fdc
RAMJI T B <113838620+ramjibalakrishna@users.noreply.github.com>
2024-01-04 04:36:36 +0530

    js page creation and header of appjs is completed

---
+++ src/Company.js
@@ -0,0 +1,10 @@
+export default function Company() {
+    return (
+      <body>
+        
+        
+  
+      </body>
+    );
+  }
+  
---
+++ src/Partners.js
@@ -0,0 +1,10 @@
+export default function Partners() {
+    return (
+      <body>
+        
+        
+  
+      </body>
+    );
+  }
+  
